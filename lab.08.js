d3f6de96136ac3bd162d12b11cde65b4523e9859
Akaki22 <ag2795@njit.edu>
2024-11-14 23:55:54 -0500

    lab8

---
+++ lab.08.js
@@ -0,0 +1,97 @@
+let name="Akaki"
+let lastname="Gelbakhiani"
+console.log("my name is ", name,lastname)
+
+
+let age="22"
+let isstudent=true
+console.log("age", age)
+console.log("isstudent", isstudent)
+
+
+let rollnumber=15
+if(rollnumber>10){
+    console.log("The RollNumber is greater than 10")
+}
+
+
+
+let ismember="true"
+if(ismember){
+    console.log("Member discount applied")
+}
+
+
+
+let startvalue="50"
+if (startvalue>0){
+    console.log
+}
+else if (startvalue<0){
+    console.log
+}
+else{
+    console.log
+}
+
+
+let a=10
+let b=5
+
+console.log("a!=b", a!=b)
+console.log("a!==b", a!==b)
+console.log("a==b", a==b)
+console.log("a==b", a===b)
+console.log("a>b", a>b)
+console.log("a<b", a<b)
+console.log("a>=b", a>=b)
+console.log("a<=b", a<=b)
+
+
+/*for some reason Q8 was not working I don't know what I did wrong:( */
+
+/*first_name = "akaki"
+last_name = "gelbakhiani"
+age = 22
+
+message= f;"hello, {first_name} {last_name}, you are {age} years old."
+print(message)*/
+
+
+let num = 50;
+if (num > 0 && num < 100) {
+    console.log("The number is within range.");
+} else {
+    console.log("The number is out of range.");
+}
+
+
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+
+if (hasCar && hasLicense && hasInsurance) {
+    console.log("You can drive legally.");
+} else {
+    console.log("Check your driving eligibility.");
+}
+
+
+let num1=21
+
+if (num % 2===0) {
+    console.log("the Number is even")
+
+} else{
+    console.log("the number is odd")
+}
+
+
+let score=85
+if(score >=90){
+    console.log("grade A.")
+} else if (score >= 80 && score <=89){
+    console.log("grade B.")
+} else {
+    console.log("Grade C.");
+}
